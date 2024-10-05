66a2ba430893c6c223e1f0a83636e9b7d9e7db96
Bruno Alencar <joaobrunoalencar@gmail.com>
2024-10-05 20:29:18 -0300

    feat: :sparkles: add route of users get from mongodb

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = 8080;
-
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+const URI = process.env.MONGODB_URI;
+const client = new MongoClient(URI,);
+const DATABASE = process.env.MONGODB_NAME;
+
+app.get('/users', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db(DATABASE);
+    const collection = database.collection('users');
+    const users = await collection.find({}).toArray();
+    res.json(users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  } finally {
+    await client.close();
+  }
 });
 
 app.listen(port, () => {
