e9072e02a20e9612bda6927fd67d4962268fb8b6
Bruno Alencar <joaobrunoalencar@gmail.com>
2024-11-02 14:26:36 -0300

    chore: docker config files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.use(morgan('combined'));
 app.get('/', (req, res) => {
